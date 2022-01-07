da492720a95f117781c14238cfe09828058ef4fb
Davut <sergeevdavut@mail.ru>
2022-01-07 17:07:04 +0300

    Added base migrations

---
+++ database/migrations/2022_01_07_124131_create_bots_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bots', function (Blueprint $table) {
+            $table->id();
+            $table->string('local_name');
+            $table->string('link')->nullable(true);
+            $table->string('api_key')->nullable(true);
+            $table->enum('status', ['PENDING', 'CREATED', 'DELETED']);
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->json('config')->nullable(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bots');
+    }
+}
