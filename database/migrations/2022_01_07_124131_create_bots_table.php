b8db1e150015c61d1aabfa2d56ec9b2410842a55
Davut <sergeevdavut@mail.ru>
2022-01-08 02:46:18 +0300

    Fix

--- database/migrations/2022_01_07_124131_create_bots_table.php
+++ database/migrations/2022_01_07_124131_create_bots_table.php
@@ -16,7 +16,7 @@ class CreateBotsTable extends Migration
         Schema::create('bots', function (Blueprint $table) {
             $table->id();
             $table->string('local_name');
-            $table->string('link');
+            $table->string('link')->nullable(true);
             $table->string('api_key');
             $table->enum('status', ['PENDING', 'CREATED', 'DELETED', 'WITH_ERRORS']);
             $table->unsignedBigInteger('user_id');
