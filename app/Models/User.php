201c75c0419442232d4b812131cc16f9cc42876f
Davut <sergeevdavut@mail.ru>
2023-12-14 00:30:08 +0300

    Added permissions package

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Collection;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasPermissions;
 
 /**
  * Class User
@@ -28,6 +29,7 @@ class User extends Authenticatable
     use HasApiTokens;
     use HasFactory;
     use Notifiable;
+    use HasPermissions;
 
     /**
      * The attributes that are mass assignable.
